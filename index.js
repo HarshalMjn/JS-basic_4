1454d8e2171a2b70010d3bd40d83e86b7661ea24
HarhslaMjn <mahajanh006@gmail.com>
2023-07-05 15:49:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+// console.log('j1 zl ka');
+
+
+// run();
+// function run() {
+//     console.log('running');
+// }
+
+// function call  or invoke
+
+
+//Named function assignment
+// let stand = function walk() {
+//     console.log('walking');
+// };
+
+//Anonymous function assignment
+// let stand2 = function() {
+//     console.log('walking');
+// };
+
+// stand2();
+
+// let jump = stand;
+
+// jump();
+
+let x = 1;
+ x = 'a';
+  console.log(x);
+
+  function sum(a,b){
+    return a+b;
+
+  };
+
+//   console.log(sum(1,2));
+console.log(sum(1));
+
+
